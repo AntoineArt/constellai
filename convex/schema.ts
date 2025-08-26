6e1958858e91787252125de621b9ac7108d855a5
Antars <artillan.antoine@hotmail.fr>
2025-08-26 10:52:26 +0200

    format

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,9 +2,9 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
-	users: defineTable({
-		name: v.string(),
-		email: v.string(),
-		createdAt: v.number(),
-	}).index("by_email", ["email"]),
+  users: defineTable({
+    name: v.string(),
+    email: v.string(),
+    createdAt: v.number(),
+  }).index("by_email", ["email"]),
 });
